93075aaca5e44b42a5f9d5c30ae45f743d478390
Piotr Piasta(Piotr Piasta)[ppiasta] <piastapiotr010@gmail.com>
2023-03-04 15:50:42 +0100

    Task list No.2

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+"""
+This module defines a simple Flask application.
+"""
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    """
+    This function returns a simple greeting message
+    """
+    return "Hello World!"
+
+@app.route("/hello", methods=["GET", "POST"])
+def hello_name():
+    """
+    This function returns a personalized greeting message
+    """
+    if request.method == "POST" and "name" in request.form:
+        name = request.form["name"]
+        message = f"Hello {name}!"
+        return render_template("hello.html", message=message)
+    return render_template("hello.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
